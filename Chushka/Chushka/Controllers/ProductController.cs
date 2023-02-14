50cdfafee264d0192971682435200799604569f2
IvailoTonchev <ivcho465@gmail.com>
2023-02-14 08:45:24 +0200

    edit

---
+++ Chushka/Chushka/Controllers/ProductController.cs
@@ -0,0 +1,52 @@
+﻿
+using Chushka.Data;
+using Chushka.Data.Models;
+using Chushka.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chushka.Controllers
+{
+    public class ProductController : Controller
+    {
+       
+            private readonly ApplicationDbContext db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+            var products = db.Products.Select(x => new InputProductModel
+            {
+                Id = x.Id,
+                Name = x.Name,
+                Price = x.Price,
+                Description = x.Description,
+                Type= (InputProductModel.Type_)x.Type,
+            }).ToList();
+
+            return View(products);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+        [HttpPost]
+        public IActionResult Create(InputProductModel model)
+        {
+            var product = new Product { Name = model.Name,
+            Id=model.Id,
+            Description=model.Description,
+            Price = model.Price,
+            Type= (Product.Type_)model.Type,
+            Orders=model.Orders
+            };
+            db.Products.Add(product);
+            db.SaveChanges();
+
+            return this.Redirect("Index");
+        }
+    }
+}
---
+++ Chushka/Chushka/Models/InputProductModel.cs
@@ -0,0 +1,15 @@
+﻿using Chushka.Data.Models;
+
+namespace Chushka.Models
+{
+    public class InputProductModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public string Description { get; set; }
+        public enum Type_ { Food = 1, Domestic = 2, Health = 3, Cosmetic = 4, Other = 5 }
+        public Type_ Type{get; set;}
+        public ICollection<ProductOrder> Orders { get; internal set; }
+    }
+}
