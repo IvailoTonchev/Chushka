15d2f538e5caf5bca466a6158a05a4bac8358aaa
IvailoTonchev <ivcho465@gmail.com>
2023-03-22 13:51:09 +0200

    edit

---
+++ Chushka-Ivaylo/Chushka/Controllers/ProductController.cs
@@ -0,0 +1,92 @@
+﻿
+using Chushka.Data;
+using Chushka.Data.Models;
+using Chushka.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chushka.Controllers
+{
+    public class ProductController : Controller
+    {
+
+        private readonly ApplicationDbContext db;
+
+        public ProductController(ApplicationDbContext db)
+        {
+            this.db = db;
+        }
+        public IActionResult Index()
+        {
+
+            return View();
+        }
+        public IActionResult Delete()
+        {
+
+            return View();
+        }
+        public IActionResult Details(int Id)
+        {
+            var model = db.Products.Where(x=>x.Id==Id).Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description = x.Description,
+                Id = x.Id,
+                Price = x.Price,
+
+            }).FirstOrDefault();
+            return View(model);
+
+        }
+        public IActionResult Detailsadmin()
+        {
+
+            return View();
+        }
+        public IActionResult Update()
+        {
+
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return this.View();
+        }
+        [HttpPost]
+        public IActionResult Create(InputProductModel input)
+        {
+            var prdouct = new Product 
+            {
+            Name = input.Name,
+            Price = input.Price,
+            Description = input.Description,
+            Type= (Product.Type_)input.ProductType,
+            };
+            db.Products.Add(prdouct);
+            db.SaveChanges();
+            return Redirect("/");
+        }
+        [HttpGet]
+        public IActionResult AdminHome()
+        {
+            var model = db.Products.Select(x => new ProductViewModel
+            {
+                Name = x.Name,
+                Description=x.Description,
+                Id=x.Id,
+                Price=x.Price
+            }).ToList();
+            return View(model);
+        }
+        [HttpPost]
+        public IActionResult AdminHome(ProductViewModel x)
+        {
+            return View();
+        }
+        public IActionResult Order()
+        {
+            return RedirectToAction("All","Order");
+        }
+    }
+}
---
+++ Chushka-Ivaylo/Chushka/Models/OrderViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Chushka.Data.Models;
+
+namespace Chushka.Models
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public ICollection<ProductOrder> Product { get; set; }
+        public bool OrderedOn { get; set; }
+        public int ClientId{ get; set;}
+        }
+}
